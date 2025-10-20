74e68a7d814a392a4330042ecbb22bc2ae260561
Habib. <habibbbahmad@gmail.com>
2025-10-20 09:58:47 +0700

    Add files via upload

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
     res.json({pesan: "halo pesan dari backend"});
-
+});
 app.post("/api/data", (req, res) => {
     const { frontend } = req.body;
     res.json({pesan: `halo ${frontend}, data diterima`});
